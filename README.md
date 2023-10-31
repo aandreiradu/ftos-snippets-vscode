28628562ee77d736b7e633a9908e2fd1814da56b
Andrei Radu <raduandrei697@gmail.com>
2023-10-31 10:27:59 +0200

    initial release

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
+<p align="center">
+    <p align="center">
+        <img width="350" height="350" src="./images/logo_whitebg.jpg" alt="Logo" />
+    </p>
+    <h1 align="center"><b>FTOS SDK Code Snippets</b></h1>
+</p>
+
 ## Installation
 
 1. Launch _Quick Open_:
 
    - Linux & Windows: `Ctrl+P`
    - macOS: `⌘+P`
+
+## Snippets
+
+| Keyword                   | Description                                |
+| ------------------------- | ------------------------------------------ |
+| ftos:server:library       | import and cache a server library          |
+| ftos:server:query         | import and cache a server library          |
+| ftos:monitoring:deadlocks | Returns the deadlocks identifier procedure |
+| ftos:monitoring:lockchain | Returns the lockchain identifier procedure |
--- scripts/generate-readme.ts
+++ scripts/generate-readme.ts
@@ -7,9 +7,39 @@ interface CodeSnippet {
   description: string;
 }
 
-const pathToSnippetsPath = path.join(
-  __dirname,
-  "../snippets/ftos-snippets.json",
-);
-const jsonPackagePath = path.join(__dirname, "../package.json");
+const snippetsPath = path.join(__dirname, "../snippets/ftos-snippets.json");
+const packageJsonPath = path.join(__dirname, "../package.json");
 const templateFilePath = path.join(__dirname, "../templates/readme.md");
+const readMeFilePath = path.join(__dirname, "../README.md");
+const tableArr = ["| Keyword | Description |\n", "|---------|-------------|\n"];
+
+async function main() {
+  const [snippetsContent, packageJsonContent, templateContent] =
+    await Promise.all([
+      fs.readFile(snippetsPath, "utf-8"),
+      fs.readFile(packageJsonPath, "utf-8"),
+      fs.readFile(templateFilePath, "utf-8"),
+    ]);
+
+  const { prettier: prettierOptions } = JSON.parse(packageJsonContent) as {
+    prettier: Config;
+  };
+  prettierOptions.parser = "markdown";
+
+  const snippets = Object.values(JSON.parse(snippetsContent)) as CodeSnippet[];
+  for (const snippet of snippets) {
+    tableArr.push(`| ${snippet.prefix} | ${snippet.description} |\n`);
+  }
+
+  const table = tableArr.join("");
+  const content = templateContent.replace("{{ SNIPPETS-TABLE }}", table);
+  const readMe = await format(content, prettierOptions);
+  await fs.writeFile(readMeFilePath, readMe);
+}
+
+try {
+  main();
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
